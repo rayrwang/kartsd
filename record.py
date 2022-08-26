f9e0444f3ab6e9b1d151e5922a2feec75e62223a
Raymond <rwang7839@icloud.com>
2022-08-25 21:08:05 -0400

    Change recording fps from 30 to 5

--- record.py
+++ record.py
@@ -3,6 +3,7 @@ Record video to train.csv
 """
 
 import math
+import time
 
 import cv2 as cv
 import numpy as np
@@ -10,13 +11,11 @@ import pygame as pg
 
 import hardware
 
-cap, board, angle_region, angle_read, last, window, font0, font1, font2, update = hardware.init_hardware(update_msec=1000)
+cap, board, angle_region, angle_read, last, window, font0, font1, font2, update = hardware.init_hardware(update_msec=200)
 
+start = time.perf_counter()
 with open("train.csv", "a") as file:
-    while True:
-        _, img = cap.read(0)
-        cv.imshow("", img)
-
+    while time.perf_counter() - start < 10:
         if cv.waitKey(1) == ord("f"):
             cv.destroyAllWindows() 
             cv.VideoCapture(0).release()
@@ -24,13 +23,17 @@ with open("train.csv", "a") as file:
 
         angle_region, angle_read, last, degree = hardware.update_angle(board, angle_region, angle_read, last)
 
-        flat = img.reshape(36864)
-        flat = flat.astype("int16")
-        full = np.insert(flat, 0, degree)
-
-        np.savetxt(file, [full], fmt="%.0f", delimiter=",", newline="")
-        file.write("\n")
-
         for event in pg.event.get():
             if event.type == update:
+                # Update display and record frame at same time
+                _, img = cap.read(0)
+                cv.imshow("", img)
+
+                flat = img.reshape(36864)
+                flat = flat.astype("int16")
+                full = np.insert(flat, 0, degree)
+
+                np.savetxt(file, [full], fmt="%.0f", delimiter=",", newline="")
+                file.write("\n")
+
                 hardware.update_display(window, font0, font1, font2, angle_region, angle_read, degree)
