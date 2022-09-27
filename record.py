90ffa9e372ba165f6b5aece588023023de64dfac
Raymond Wang <rwang7839@icloud.com>
2022-09-27 00:18:58 -0400

    Try threading to avoid crash

--- record.py
+++ record.py
@@ -2,14 +2,26 @@
 Record video to train.csv
 """
 
+import threading
+
 import cv2
 import numpy as np
 import pygame as pg
 
 import hardware
 
-cap0, cap1, cap2, board, angle_region, angle_read, last, window, font0, font1, font2, update = hardware.init_hardware(update_msec=33)
+def capture(cap0, cap1, cap2):
+    while True:
+        global img0
+        global img1
+        global img2
+        _, img0 = cap0.read(0)
+        _, img1 = cap1.read(0)
+        _, img2 = cap2.read(0)
 
+cap0, cap1, cap2, board, angle_region, angle_read, last, window, font0, font1, font2, update = hardware.init_hardware(update_msec=33)
+capture_thread = threading.Thread(target=capture, args=[cap0, cap1, cap2])
+capture_thread.start()
 with open("train.csv", "a") as file:
     while True:
         if cv2.waitKey(1) == ord("f"):
@@ -22,13 +34,10 @@ with open("train.csv", "a") as file:
         for event in pg.event.get():
             if event.type == update:
                 # Update display and record frame at same time
-                _, img0 = cap0.read()
                 cv2.imshow("0", img0)
 
-                _, img1 = cap1.read()
                 cv2.imshow("1", img1)
 
-                _, img2 = cap2.read()
                 cv2.imshow("2", img2)
 
                 # flat = img.reshape(36864)
