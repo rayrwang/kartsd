cd265f1f431e260d98ce3116cdf57d335da032a1
Raymond Wang <rwang7839@icloud.com>
2022-09-27 01:04:10 -0400

    Update record.py

--- record.py
+++ record.py
@@ -19,9 +19,7 @@ def capture(cap0, cap10, cap11, cap2):
         global img2
         _, img0 = cap0.read(0)
         _, img1 = cap10.read(0)
-        print(_)
         if not _:
-            print("backup")
             _, img1 = cap11.read(0)
         _, img2 = cap2.read(0)
 
