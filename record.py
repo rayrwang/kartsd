edadaca0a7b9053f8bfeae25a82b7e259e40143f
Raymond <rwang7839@icloud.com>
2022-09-30 01:19:19 -0400

    Revert resolution

--- record.py
+++ record.py
@@ -22,7 +22,7 @@ def capture(cap0, cap1, cap2):
         _, img2 = cap2.read(0)
 
 
-cap0, cap1, cap2, board, angle_region, angle_read, last, window, font0, font1, font2, update = hardware.init_hardware(update_msec=1000)
+cap0, cap1, cap2, board, angle_region, angle_read, last, window, font0, font1, font2, update = hardware.init_hardware(update_msec=500)
 
 capture_thread = threading.Thread(target=capture, args=[cap0, cap1, cap2])
 capture_thread.start()
@@ -41,7 +41,7 @@ with open("train.csv", "a") as file:
                 cv2.imshow("1", img1)
                 cv2.imshow("2", img2)
 
-                flat0 = img0.reshape(230400)
+                flat0 = img0.reshape(36864)
                 flat1 = img1.reshape(76032)
                 flat2 = img2.reshape(76032)
                 full = np.concatenate((flat0, flat1, flat2))
