0d6c547ce7c6ed8fbcc0ba3a7eca1cb80dd0c977
Raymond Wang <rwang7839@icloud.com>
2022-09-29 22:55:45 -0400

    3 images labeler

--- labeler.py
+++ labeler.py
@@ -6,25 +6,175 @@ import cv2
 import pygame as pg
 
 
-def project(*points):
+def project_center(*points):
     for point in points:
-        att = math.pi / 180 * 0.38 * (point[1] + 8)  # + difference between horizon and index at top of (used portion) of image
+        att = math.pi / 180 * 0.38 * (point[1] + 8)  # + difference between index at top of (used portion) of image and horizon
         azi = math.pi / 180 * 0.38 * (point[0] - 63.5)  # - index at center of image
         dist = 0.7 / math.tan(att)  # dist : distance on ground from camera to px location
         x = dist * math.sin(azi)
         y = dist * math.cos(azi)
 
-        vs_x = round((x + 5.0625) / 0.125)  # Adjust x to move 0 from center to left
+        vs_x = round((x + 6.3125) / 0.125)  # Adjust x to move 0 from center to left
         vs_y = round((y - 0.0625) / 0.125)  # Pull y half a vs block down
-        if 0 <= vs_x < 81:
+        if 0 <= vs_x < 101 and 0 <= vs_y < 120:
             vs[vs_y, vs_x] = 1
 
 
-window = pg.display.set_mode((405, 655))
+def project_left(*points):
+    for point in points:
+        att = math.pi / 180 * 0.35 * (point[1] + 0)  # + difference between index at top of (used portion) of image and horizon
+        azi = math.pi / 180 * 0.35 * (point[0] - 87.5)  # - index at center of image
+        try:
+            dist = 0.805 / math.tan(att)  # dist : distance on ground from camera to px location
+        except:
+            return
+        x_old = dist * math.sin(azi)
+        y_old = dist * math.cos(azi)
+
+        # Correct for camera position and orientation relative to center camera
+        x = x_old*math.cos(45 * math.pi/180) - y_old*math.sin(45 * math.pi/180) - 0.04
+        y = x_old*math.sin(45 * math.pi/180) + y_old*math.cos(45 * math.pi/180) - 0.05
+
+        vs_x = round((x + 6.3125) / 0.125)  # Adjust x to move 0 from center to left
+        vs_y = round((y - 0.0625) / 0.125)  # Pull y half a vs block down
+        if 0 <= vs_x < 101 and 0 <= vs_y < 120:
+            vs[vs_y, vs_x] = 1
+
+
+def project_right(*points):
+    for point in points:
+        att = math.pi / 180 * 0.35 * (point[1] + 0)  # + difference between index at top of (used portion) of image and horizon
+        azi = math.pi / 180 * 0.35 * (point[0] - 87.5)  # - index at center of image
+        try:
+            dist = 0.805 / math.tan(att)  # dist : distance on ground from camera to px location
+        except:
+            return
+        x_old = dist * math.sin(azi)
+        y_old = dist * math.cos(azi)
+
+        # Correct for camera position and orientation relative to center camera
+        x = x_old*math.cos(-45 * math.pi/180) - y_old*math.sin(-45 * math.pi/180) + 0.04
+        y = x_old*math.sin(-45 * math.pi/180) + y_old*math.cos(-45 * math.pi/180) - 0.05
+
+        vs_x = round((x + 6.3125) / 0.125)  # Adjust x to move 0 from center to left
+        vs_y = round((y - 0.0625) / 0.125)  # Pull y half a vs block down
+        if 0 <= vs_x < 101 and 0 <= vs_y < 120:
+            vs[vs_y, vs_x] = 1
+
+
+def draw_img0(event, x, y, *args, **kwargs):
+    global l_down
+    global r_down
+    global edge_img_changed
+    if event == cv2.EVENT_LBUTTONDOWN:
+        l_down = True
+    elif event == cv2.EVENT_LBUTTONUP:
+        l_down = False
+    elif event == cv2.EVENT_RBUTTONDOWN:
+        r_down = True
+    elif event == cv2.EVENT_RBUTTONUP:
+        r_down = False
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if l_down:
+            edges_img0[y, x] = 255
+            edge_img_changed = True
+
+
+def draw_img1(event, x, y, *args, **kwargs):
+    global l_down
+    global r_down
+    global edge_img_changed
+    if event == cv2.EVENT_LBUTTONDOWN:
+        l_down = True
+    elif event == cv2.EVENT_LBUTTONUP:
+        l_down = False
+    elif event == cv2.EVENT_RBUTTONDOWN:
+        r_down = True
+    elif event == cv2.EVENT_RBUTTONUP:
+        r_down = False
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if l_down:
+            edges_img1[y, x] = 255
+            edge_img_changed = True
+
+
+def draw_img2(event, x, y, *args, **kwargs):
+    global l_down
+    global r_down
+    global edge_img_changed
+    if event == cv2.EVENT_LBUTTONDOWN:
+        l_down = True
+    elif event == cv2.EVENT_LBUTTONUP:
+        l_down = False
+    elif event == cv2.EVENT_RBUTTONDOWN:
+        r_down = True
+    elif event == cv2.EVENT_RBUTTONUP:
+        r_down = False
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if l_down:
+            edges_img2[y, x] = 255
+            edge_img_changed = True
+
+
+def draw_edge0(event, x, y, *args, **kwargs):
+    global l_down
+    global r_down
+    global edge_img_changed
+    if event == cv2.EVENT_LBUTTONDOWN:
+        l_down = True
+    elif event == cv2.EVENT_LBUTTONUP:
+        l_down = False
+    elif event == cv2.EVENT_RBUTTONDOWN:
+        r_down = True
+    elif event == cv2.EVENT_RBUTTONUP:
+        r_down = False
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if r_down:
+            edges_img0[y - 5:y + 5, x - 5:x + 5] = 0
+            edge_img_changed = True
+
+
+def draw_edge1(event, x, y, *args, **kwargs):
+    global l_down
+    global r_down
+    global edge_img_changed
+    if event == cv2.EVENT_LBUTTONDOWN:
+        l_down = True
+    elif event == cv2.EVENT_LBUTTONUP:
+        l_down = False
+    elif event == cv2.EVENT_RBUTTONDOWN:
+        r_down = True
+    elif event == cv2.EVENT_RBUTTONUP:
+        r_down = False
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if r_down:
+            edges_img1[y - 5:y + 5, x - 5:x + 5] = 0
+            edge_img_changed = True
+
+
+def draw_edge2(event, x, y, *args, **kwargs):
+    global l_down
+    global r_down
+    global edge_img_changed
+    if event == cv2.EVENT_LBUTTONDOWN:
+        l_down = True
+    elif event == cv2.EVENT_LBUTTONUP:
+        l_down = False
+    elif event == cv2.EVENT_RBUTTONDOWN:
+        r_down = True
+    elif event == cv2.EVENT_RBUTTONUP:
+        r_down = False
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if r_down:
+            edges_img2[y - 5:y + 5, x - 5:x + 5] = 0
+            edge_img_changed = True
+
+
+window = pg.display.set_mode((505, 655))
 pg.init()
 
 # Load data
-vid_arr = np.loadtxt("rawvids/testvid.csv", dtype="float16", delimiter=",")
+vid_arr = np.loadtxt("rawvids/three1.csv", dtype="float16", delimiter=",")
 # steer_arr = vid_arr[:, 0]
 # vid_arr = np.delete(vid_arr, 0, axis=1)
 vid_arr = vid_arr.astype("uint8")
@@ -37,8 +187,6 @@ prev_img_num = -1
 # 2 evens complete
 
 img_num = 0
-prev_lower, prev_upper = 30000, 40000
-lower, upper = 30000, 40000
 
 cv2.namedWindow("1", cv2.WINDOW_NORMAL)
 cv2.resizeWindow("1", 512, 384)
@@ -53,9 +201,20 @@ cv2.resizeWindow("5", 512, 384)
 cv2.namedWindow("6", cv2.WINDOW_NORMAL)
 cv2.resizeWindow("6", 512, 384)
 
+cv2.setMouseCallback("1", draw_img0)
+cv2.setMouseCallback("2", draw_img1)
+cv2.setMouseCallback("3", draw_img2)
+cv2.setMouseCallback("4", draw_edge0)
+cv2.setMouseCallback("5", draw_edge1)
+cv2.setMouseCallback("6", draw_edge2)
+
+l_down = False
+r_down = False
+edge_img_changed = False
+
 car = pg.Surface((35, 55))
 
-vs = np.zeros((120, 81))
+vs = np.zeros((120, 101))
 with open(r"vstrainingdata/vs_train_rough.csv", "a") as file:
     while True:
         save = False
@@ -71,14 +230,6 @@ with open(r"vstrainingdata/vs_train_rough.csv", "a") as file:
                     save = True
                 elif keys[pg.K_a]:
                     img_num -= 1
-                elif keys[pg.K_r]:
-                    lower += 5000
-                elif keys[pg.K_f]:
-                    lower -= 5000
-                elif keys[pg.K_t]:
-                    upper += 5000
-                elif keys[pg.K_g]:
-                    upper -= 5000
 
         # Handle mouse click to flip pixels
         buttons = pg.mouse.get_pressed(num_buttons=3)
@@ -90,19 +241,23 @@ with open(r"vstrainingdata/vs_train_rough.csv", "a") as file:
         if buttons[0]:
             vs[119 - y, x] = 1
         elif buttons[1]:
-            vs[119-y-3: 119-y+3, x-3: x+3] = 0
+            vs[119 - y - 3: 119 - y + 3, x - 3: x + 3] = 0
         elif buttons[2]:
             vs[119 - y, x] = 0
 
-        if img_num != prev_img_num or lower != prev_lower or upper != prev_upper:
-            prev_lower = lower
-            prev_upper = upper
+        if edge_img_changed:
+            cv2.imshow("4", edges_img0)
+            cv2.imshow("5", edges_img1)
+            cv2.imshow("6", edges_img2)
 
+        if img_num != prev_img_num:
             # Write previous completed image and vs to file
             if save:
-                img = img.reshape(36864)
+                img0 = img0.reshape(36864)
+                img1 = img1.reshape(36864)
+                img2 = img2.reshape(36864)
                 vs = vs.reshape(4270)
-                full = np.concatenate((img, vs))
+                full = np.concatenate((img0, img1, img2, vs))
                 np.savetxt(file, [full], fmt="%.0f", delimiter=",")
 
             # Get new image
@@ -119,45 +274,102 @@ with open(r"vstrainingdata/vs_train_rough.csv", "a") as file:
             cv2.imshow("3", img2)
 
             # Compute edges
-            edges_img0 = cv2.Canny(img0, lower, upper, apertureSize=7, L2gradient=True)
-            edges_img0[:25] = 0
+            edges_img0 = cv2.Canny(img0, 999999, 999999, apertureSize=7, L2gradient=True)
             cv2.imshow("4", edges_img0)
-            edges_img1 = cv2.Canny(img1, lower, upper, apertureSize=7, L2gradient=True)
-            edges_img1[:25] = 0
+            edges_img1 = cv2.Canny(img1, 999999, 999999, apertureSize=7, L2gradient=True)
             cv2.imshow("5", edges_img1)
-            edges_img2 = cv2.Canny(img2, lower, upper, apertureSize=7, L2gradient=True)
-            edges_img2[:25] = 0
+            edges_img2 = cv2.Canny(img2, 999999, 999999, apertureSize=7, L2gradient=True)
             cv2.imshow("6", edges_img2)
-            print(img_num, vid_arr.shape[0], lower, upper)
+            print(img_num, vid_arr.shape[0])
 
-            # # Compute physical x and y for pixels in edges
-            # vs = np.zeros((120, 81))  # rows, columns
-            # for px_y, row in enumerate(edges_img[25:]):  # px_y : pixels below horizon
-            #     for px_x, pos in enumerate(row):  # px_x : pixels from left (48 to center)
-            #         if pos == 255:
-            #             project((px_x, px_y),
-            #                     (px_x + 0.1, px_y + 0.1), (px_x - 0.1, px_y + 0.1),
-            #                     (px_x + 0.2, px_y + 0.2), (px_x - 0.2, px_y + 0.2),
-            #                     (px_x + 0.3, px_y + 0.3), (px_x - 0.3, px_y + 0.3),
-            #                     (px_x + 0.4, px_y + 0.4), (px_x - 0.4, px_y + 0.4),
-            #                     (px_x + 0.5, px_y + 0.5), (px_x - 0.5, px_y + 0.5),
-            #                     (px_x - 0.1, px_y - 0.1), (px_x + 0.1, px_y - 0.1),
-            #                     (px_x - 0.2, px_y - 0.2), (px_x + 0.2, px_y - 0.2),
-            #                     (px_x - 0.3, px_y - 0.3), (px_x + 0.3, px_y - 0.3),
-            #                     (px_x - 0.4, px_y - 0.4), (px_x + 0.4, px_y - 0.4),
-            #                     (px_x - 0.5, px_y - 0.5), (px_x + 0.5, px_y - 0.5))
-
+        if img_num != prev_img_num or edge_img_changed:
+            edge_img_changed = False
             prev_img_num = img_num
+
+            # Compute physical x and y for pixels in edges
+            vs = np.zeros((120, 101))  # rows, columns
+            for px_y, row in enumerate(edges_img0[25:]):  # px_y : pixels below horizon
+                for px_x, pos in enumerate(row):  # px_x : pixels from left (48 to center)
+                    if pos == 255:
+                        project_center((px_x, px_y),
+                                       (px_x + 0.1, px_y), (px_x - 0.1, px_y),
+                                       (px_x + 0.2, px_y), (px_x - 0.2, px_y),
+                                       (px_x + 0.3, px_y), (px_x - 0.3, px_y),
+                                       (px_x + 0.4, px_y), (px_x - 0.4, px_y),
+                                       (px_x + 0.5, px_y), (px_x - 0.5, px_y),
+                                       (px_x, px_y + 0.1), (px_x, px_y - 0.1),
+                                       (px_x, px_y + 0.2), (px_x, px_y - 0.2),
+                                       (px_x, px_y + 0.3), (px_x, px_y - 0.3),
+                                       (px_x, px_y + 0.4), (px_x, px_y - 0.4),
+                                       (px_x, px_y + 0.5), (px_x, px_y - 0.5),
+                                       (px_x + 0.1, px_y + 0.1), (px_x - 0.1, px_y + 0.1),
+                                       (px_x + 0.2, px_y + 0.2), (px_x - 0.2, px_y + 0.2),
+                                       (px_x + 0.3, px_y + 0.3), (px_x - 0.3, px_y + 0.3),
+                                       (px_x + 0.4, px_y + 0.4), (px_x - 0.4, px_y + 0.4),
+                                       (px_x + 0.5, px_y + 0.5), (px_x - 0.5, px_y + 0.5),
+                                       (px_x - 0.1, px_y - 0.1), (px_x + 0.1, px_y - 0.1),
+                                       (px_x - 0.2, px_y - 0.2), (px_x + 0.2, px_y - 0.2),
+                                       (px_x - 0.3, px_y - 0.3), (px_x + 0.3, px_y - 0.3),
+                                       (px_x - 0.4, px_y - 0.4), (px_x + 0.4, px_y - 0.4),
+                                       (px_x - 0.5, px_y - 0.5), (px_x + 0.5, px_y - 0.5))
+            for px_y, row in enumerate(edges_img1[85:]):  # px_y : pixels below horizon
+                for px_x, pos in enumerate(row):  # px_x : pixels from left (48 to center)
+                    if pos == 255:
+                        project_right((px_x, px_y),
+                                     (px_x + 0.1, px_y), (px_x - 0.1, px_y),
+                                     (px_x + 0.2, px_y), (px_x - 0.2, px_y),
+                                     (px_x + 0.3, px_y), (px_x - 0.3, px_y),
+                                     (px_x + 0.4, px_y), (px_x - 0.4, px_y),
+                                     (px_x + 0.5, px_y), (px_x - 0.5, px_y),
+                                     (px_x, px_y + 0.1), (px_x, px_y - 0.1),
+                                     (px_x, px_y + 0.2), (px_x, px_y - 0.2),
+                                     (px_x, px_y + 0.3), (px_x, px_y - 0.3),
+                                     (px_x, px_y + 0.4), (px_x, px_y - 0.4),
+                                     (px_x, px_y + 0.5), (px_x, px_y - 0.5),
+                                     (px_x + 0.1, px_y + 0.1), (px_x - 0.1, px_y + 0.1),
+                                     (px_x + 0.2, px_y + 0.2), (px_x - 0.2, px_y + 0.2),
+                                     (px_x + 0.3, px_y + 0.3), (px_x - 0.3, px_y + 0.3),
+                                     (px_x + 0.4, px_y + 0.4), (px_x - 0.4, px_y + 0.4),
+                                     (px_x + 0.5, px_y + 0.5), (px_x - 0.5, px_y + 0.5),
+                                     (px_x - 0.1, px_y - 0.1), (px_x + 0.1, px_y - 0.1),
+                                     (px_x - 0.2, px_y - 0.2), (px_x + 0.2, px_y - 0.2),
+                                     (px_x - 0.3, px_y - 0.3), (px_x + 0.3, px_y - 0.3),
+                                     (px_x - 0.4, px_y - 0.4), (px_x + 0.4, px_y - 0.4),
+                                     (px_x - 0.5, px_y - 0.5), (px_x + 0.5, px_y - 0.5))
+            for px_y, row in enumerate(edges_img2[85:]):  # px_y : pixels below horizon
+                for px_x, pos in enumerate(row):  # px_x : pixels from left (48 to center)
+                    if pos == 255:
+                        project_left((px_x, px_y),
+                                      (px_x + 0.1, px_y), (px_x - 0.1, px_y),
+                                      (px_x + 0.2, px_y), (px_x - 0.2, px_y),
+                                      (px_x + 0.3, px_y), (px_x - 0.3, px_y),
+                                      (px_x + 0.4, px_y), (px_x - 0.4, px_y),
+                                      (px_x + 0.5, px_y), (px_x - 0.5, px_y),
+                                      (px_x, px_y + 0.1), (px_x, px_y - 0.1),
+                                      (px_x, px_y + 0.2), (px_x, px_y - 0.2),
+                                      (px_x, px_y + 0.3), (px_x, px_y - 0.3),
+                                      (px_x, px_y + 0.4), (px_x, px_y - 0.4),
+                                      (px_x, px_y + 0.5), (px_x, px_y - 0.5),
+                                      (px_x + 0.1, px_y + 0.1), (px_x - 0.1, px_y + 0.1),
+                                      (px_x + 0.2, px_y + 0.2), (px_x - 0.2, px_y + 0.2),
+                                      (px_x + 0.3, px_y + 0.3), (px_x - 0.3, px_y + 0.3),
+                                      (px_x + 0.4, px_y + 0.4), (px_x - 0.4, px_y + 0.4),
+                                      (px_x + 0.5, px_y + 0.5), (px_x - 0.5, px_y + 0.5),
+                                      (px_x - 0.1, px_y - 0.1), (px_x + 0.1, px_y - 0.1),
+                                      (px_x - 0.2, px_y - 0.2), (px_x + 0.2, px_y - 0.2),
+                                      (px_x - 0.3, px_y - 0.3), (px_x + 0.3, px_y - 0.3),
+                                      (px_x - 0.4, px_y - 0.4), (px_x + 0.4, px_y - 0.4),
+                                      (px_x - 0.5, px_y - 0.5), (px_x + 0.5, px_y - 0.5))
 
         # Display vs
         window.fill((255, 255, 255))
-        window.blit(car, (185, 600))
+        window.blit(car, (235, 600))
         for n_y, y_row in enumerate(vs):
             for n_x, x in enumerate(y_row):
                 if x == 1:
                     rect = pg.Surface((5, 5))
                     pg.draw.rect(rect, (0, 0, 0), (0, 0, 10, 10))
-                    window.blit(rect, (5*n_x, 595-(5*n_y)))
+                    window.blit(rect, (5 * n_x, 595 - (5 * n_y)))
         pg.display.update()
 
         if cv2.waitKey(1) == ord("f"):
