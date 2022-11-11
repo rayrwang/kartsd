22c333b895159c0a5f49e69a8b464dc3acf828e7
Raymond Wang <rwang7839@icloud.com>
2022-11-11 17:22:50 -0500

    Remove old network

--- networks.py
+++ networks.py
@@ -3,28 +3,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class SteerNet(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-        self.conv1 = nn.Conv2d(3, 5, 5, stride=2)
-        self.pool = torch.nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(5, 10, 5, stride=2)
-        self.fc1 = nn.Linear(350, 40)
-        self.fc2 = nn.Linear(40, 40)
-        self.fc3 = nn.Linear(40, 1)
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.reshape(-1, 350)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-
-        return x
-
-
 class VSNet(nn.Module):
     def __init__(self):
         super().__init__()
