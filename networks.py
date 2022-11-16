fd10ce45bbe3acdbe90f864aa927b7cfac08f264
Raymond Wang <rwang7839@icloud.com>
2022-11-15 19:26:19 -0500

    New network for road edges & drivable surface

--- networks.py
+++ networks.py
@@ -8,58 +8,35 @@ class VSNet(nn.Module):
         super().__init__()
 
         self.dropout = nn.Dropout(p=0.5)
-        self.fc1 = nn.Linear(8192, 500)
-        self.fc2 = nn.Linear(500, 12120)
+        self.fc1 = nn.Linear(12600, 500)
+        self.fc2 = nn.Linear(500, 2*120*101)
 
-        # Networks
-        self.n1 = nn.ModuleDict(dict(
-            conv1=nn.Conv2d(3, 16, kernel_size=7, stride=(3, 4)),
-            conv2=nn.Conv2d(16, 32, kernel_size=3, stride=2),
-            conv3=nn.Conv2d(32, 64, kernel_size=3, stride=2),
-            conv4=nn.Conv2d(64, 128, kernel_size=3, stride=2),
-            act=nn.ReLU()
-        ))
-        self.n2 = nn.ModuleDict(dict(
-            conv1=nn.Conv2d(3, 32, kernel_size=7, stride=(3, 6)),
-            conv2=nn.Conv2d(32, 64, kernel_size=3, stride=2),
-            conv3=nn.Conv2d(64, 128, kernel_size=3, stride=2),
-            act=nn.ReLU()
-        ))
-        self.n3 = nn.ModuleDict(dict(
-            conv1=nn.Conv2d(3, 32, kernel_size=7, stride=(3, 6)),
-            conv2=nn.Conv2d(32, 64, kernel_size=3, stride=2),
-            conv3=nn.Conv2d(64, 128, kernel_size=3, stride=2),
-            act=nn.ReLU()
+        # ResNets
+        self.r = nn.ModuleDict(dict(
+            act=nn.ReLU(),
+            conv1=nn.Conv2d(3, 20, 7, 2),
+            conv2=nn.Conv2d(20, 20, 3, 2),
+            conv3=nn.Conv2d(20, 20, 3, 1)
         ))
 
-    def forward(self, x1, x2, x3):
-        n1 = self.n1
-        n2 = self.n2
-        n3 = self.n3
+    def forward(self, x0, x1, x2, x3, x4):
+        r = self.r
 
-        x1 = n1.act(n1.conv1(x1))
-        x1 = n1.act(n1.conv2(x1))
-        x1 = n1.act(n1.conv3(x1))
-        x1 = n1.act(n1.conv4(x1))
-        # print(x1.shape)
-        x1 = x1.reshape(-1, 3584)
+        x = [x0, x1, x2, x3, x4]
+        for i in range(5):
+            x[i] = r.act(r.conv1(x[i]))
+            x[i] = r.act(r.conv2(x[i]))
+            x[i] = r.act(r.conv2(x[i]))
+            x[i] = r.act(r.conv2(x[i]))
+            x[i] = r.act(r.conv2(x[i]))
+            x[i] = r.act(r.conv3(x[i]))
+            x[i] = r.act(r.conv3(x[i]))
+            # print(x[i].shape)
+            x[i] = x[i].reshape(-1, 2520)
 
-        x2 = n2.act(n2.conv1(x2))
-        x2 = n2.act(n2.conv2(x2))
-        x2 = n2.act(n2.conv3(x2))
-        # print(x2.shape)
-        x2 = x2.reshape(-1, 2304)
-
-        x3 = n3.act(n3.conv1(x3))
-        x3 = n3.act(n3.conv2(x3))
-        x3 = n3.act(n3.conv3(x3))
-        # print(x3.shape)
-        x3 = x3.reshape(-1, 2304)
-
-        x = torch.cat((x1, x2, x3), dim=1)
+        x = torch.cat((x[0], x[1], x[2], x[3], x[4]), dim=1)
 
         # TODO Use transformer for decoding
-
 
         x = F.relu(self.fc1(x))
         x = self.dropout(x)
